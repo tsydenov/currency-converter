8246abd0890b91decf85952677c4dc02a3ef4017
tsydenov <tsydenovs@mail.ru>
2024-03-23 14:40:15 +0700

    Adding logout route and authentication error to login route

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,12 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('auth/login.html.twig');
+        return $this->render('auth/login.html.twig', [
+            'error' => $authenticationUtils->getLastAuthenticationError()
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \Exception('/logout should never be reached!');
     }
 }
