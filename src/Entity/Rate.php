83a76c551ceaa05c4a74214e0d2e413f1c526eb6
tsydenov <tsydenovs@mail.ru>
2024-03-23 16:35:00 +0700

    Adding exchange rate entity

---
+++ src/Entity/Rate.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RateRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RateRepository::class)]
+class Rate
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 3)]
+    private ?string $base = null;
+
+    #[ORM\Column(length: 3)]
+    private ?string $quote = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 20, scale: 10)]
+    private ?string $price = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBase(): ?string
+    {
+        return $this->base;
+    }
+
+    public function setBase(string $base): static
+    {
+        $this->base = $base;
+
+        return $this;
+    }
+
+    public function getQuote(): ?string
+    {
+        return $this->quote;
+    }
+
+    public function setQuote(string $quote): static
+    {
+        $this->quote = $quote;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeImmutable $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
