5b026f743e575848b53e875ac330f21eb2791028
tsydenov <tsydenovs@mail.ru>
2024-03-23 13:44:12 +0700

    Adding authentication controller

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AuthController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function login(): Response
+    {
+        return $this->render('auth/login.html.twig');
+    }
+}
