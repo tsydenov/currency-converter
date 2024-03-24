4b396e88886b81be8128d05ed3337e2e64cac728
tsydenov <tsydenovs@mail.ru>
2024-03-24 14:35:33 +0700

    Getting and rendering exchange rates data

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\RatesSourceRepository;
+use App\Serializer\XmlRateDeserializer;
+use App\Service\RatesFetcher;
+use App\Service\RatesUpdater;
+use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +16,28 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
     #[IsGranted('ROLE_USER')]
-    public function homepage(): Response
-    {
-        return $this->render('homepage/homepage.html.twig');
+    public function index(
+        RatesUpdater $ratesUpdater,
+        RatesSourceRepository $ratesSourceRepository,
+    ): Response {
+        $ratesSource = $ratesSourceRepository->findOneBy(['source' => 'CBR']);
+        $currentTime = new DateTimeImmutable();
+
+        $timeInterval = $ratesSource->getUpdatedAt()->diff($currentTime);
+        $hoursInterval = $timeInterval->h + ($timeInterval->d * 24);
+
+        // Updating exchange rates data if last update was more than 6 hours ago
+        if ($hoursInterval >= 6) {
+            $rates = $ratesUpdater->update();
+        } else {
+            $rates = $ratesSource->getRates()->toArray();
+        }
+
+        return $this->render(
+            'homepage/homepage.html.twig',
+            [
+                'rates' => $rates
+            ]
+        );
     }
 }
